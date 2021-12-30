87bda04c712c1fd03be4907d571a6973cb899690
miretz <1217795+Miretz@users.noreply.github.com>
2021-12-30 13:39:17 +0100

    camera view - drag to move, move generator out of draw loop

--- main.go
+++ main.go
@@ -56,7 +56,67 @@ func juliaPixel(pos rl.Vector2, xdim float32, ydim float32, maxIter int) rl.Colo
 
 }
 
+func generatePixels(width, height int) []byte {
+
+	result := make([]byte, width*height*4)
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			pos := rl.NewVector2(float32(x), float32(y))
+			mPixel := mandelbrotPixel(pos,
+				float32(width),
+				float32(height),
+				30)
+			jPixel := juliaPixel(pos,
+				float32(width),
+				float32(height),
+				30)
+
+			pixelColor := rl.Black
+			if mPixel == rl.Black {
+				pixelColor = jPixel
+			} else {
+				pixelColor = mPixel
+			}
+
+			index := (y*width + x) * 4
+			result[index] = byte(pixelColor.R)
+			result[index+1] = byte(pixelColor.G)
+			result[index+2] = byte(pixelColor.B)
+			result[index+3] = byte(pixelColor.A)
+
+		}
+	}
+	return result
+}
+
+func cameraUpdate(camera *rl.Camera2D, cameraTarget rl.Vector2, windowWidth int32, windowHeight int32, previousMousePosition *rl.Vector2) rl.Vector2 {
+	const cameraMoveSpeed = 8
+
+	// Move camera with keyboard
+	if rl.IsKeyDown(rl.KeyRight) || rl.IsKeyDown(rl.KeyD) {
+		cameraTarget.X += cameraMoveSpeed
+	} else if rl.IsKeyDown(rl.KeyLeft) || rl.IsKeyDown(rl.KeyA) {
+		cameraTarget.X -= cameraMoveSpeed
+	} else if rl.IsKeyDown(rl.KeyUp) || rl.IsKeyDown(rl.KeyW) {
+		cameraTarget.Y -= cameraMoveSpeed
+	} else if rl.IsKeyDown(rl.KeyDown) || rl.IsKeyDown(rl.KeyS) {
+		cameraTarget.Y += cameraMoveSpeed
+	}
+
+	// Move camera with mouse
+	mousePos := rl.GetMousePosition()
+	delta := rl.Vector2Subtract(*previousMousePosition, mousePos)
+	*previousMousePosition = mousePos
+	if rl.IsMouseButtonDown(0) {
+		cameraTarget = rl.GetScreenToWorld2D(rl.Vector2Add(camera.Offset, delta), *camera)
+	}
+
+	return cameraTarget
+}
+
 func main() {
+
 	const defaultWindowWidth = 800
 	const defaultWindowHeight = 800
 
@@ -69,45 +129,50 @@ func main() {
 	rl.InitWindow(windowWidth, windowHeight, "Mandelbrot and Julia set visualizer")
 	rl.SetTargetFPS(60)
 
+	// Camera setup\
+	cameraTarget := rl.NewVector2(float32(windowWidth/2.0), float32(windowHeight/2.0))
+	camera := rl.Camera2D{}
+	camera.Target = cameraTarget
+	camera.Offset = rl.NewVector2(cameraTarget.X, cameraTarget.Y)
+	camera.Rotation = 0.0
+	camera.Zoom = 1.0
+
+	mousePos := rl.GetMousePosition()
+
+	// generate patterns
+
+	const drawAreaWidth = 1000
+	const drawAreaHeight = 1000
+
+	pixels := generatePixels(drawAreaWidth, drawAreaHeight)
+	image := rl.NewImage(pixels, drawAreaWidth, drawAreaHeight, 1, rl.UncompressedR8g8b8a8)
+	texture := rl.LoadTextureFromImage(image)
+
 	for !rl.WindowShouldClose() {
 
 		// Update on resize
 		windowWidth = int32(rl.GetScreenWidth())
 		windowHeight = int32(rl.GetScreenHeight())
 
+		cameraTarget = cameraUpdate(&camera, cameraTarget, windowWidth, windowHeight, &mousePos)
+		camera.Target = cameraTarget
+
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
+		rl.BeginMode2D(camera)
 
-		var y int32
-		var x int32
-		for y = 0; y < windowHeight; y++ {
-			for x = 0; x < windowWidth; x++ {
-				pos := rl.Vector2{
-					X: float32(x),
-					Y: float32(y)}
-				mPixel := mandelbrotPixel(pos,
-					float32(windowWidth),
-					float32(windowHeight),
-					30)
-				jPixel := juliaPixel(pos,
-					float32(windowWidth),
-					float32(windowHeight),
-					30)
+		rl.DrawTexture(
+			texture,
+			windowWidth/2-texture.Width/2,
+			windowHeight/2-texture.Height/2,
+			rl.White)
 
-				pixelColor := rl.Black
-				if mPixel == rl.Black {
-					pixelColor = jPixel
-				} else {
-					pixelColor = mPixel
-				}
-
-				rl.DrawPixelV(pos, pixelColor)
-			}
-		}
-
+		rl.EndMode2D()
 		rl.EndDrawing()
 
 	}
 
+	rl.UnloadImage(image)
+	rl.UnloadTexture(texture)
 	rl.CloseWindow()
 }
